59647c38a99b33cd9d1c4ba250602988a2bd9f0d
pandanoir <moon_in_france@yahoo.co.jp>
2018-02-01 22:12:09 +0900

    give a directory name to installer.sh

--- installer.sh
+++ installer.sh
@@ -1,8 +1,9 @@
 #!bin/bash
+dest=${1:-dest}
 . config.sh
 wget https://github.com/$username/$repository_name/archive/$branch.zip
 unzip $branch.zip
 rm $branch.zip
-mv $repository_name-$branch dest
-cd dest
+mv $repository_name-$branch $dest
+cd $dest
 [ -f ./init.sh ] && . ./init.sh
